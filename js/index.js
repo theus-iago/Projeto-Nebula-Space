e24c6394531bb6d88574b4b4208beea98cff0b9b
theus-iago <ms2283903@gmail.com>
2023-12-08 14:00:19 -0300

    update

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 const body = document.querySelector("body");
 const player = document.querySelector("#player");
-const enemy = document.querySelector("#enemy");
+const enemy01 = document.querySelector("#enemy01");
+const enemy02 = document.querySelector("#enemy02");
+const enemy03 = document.querySelector("#enemy03");
 const gameBoard = document.querySelector(".game-board");
 
 const positionPlayer = player.getBoundingClientRect();
-const positionEnemy = enemy.getBoundingClientRect();
+const positionenemy01 = enemy01.getBoundingClientRect();
+const positionenemy02 = enemy02.getBoundingClientRect();
+const positionenemy03 = enemy03.getBoundingClientRect();
 const positionGameBoard = gameBoard.getBoundingClientRect();
 const positionBody = body.getBoundingClientRect();
 var vidaAtual = 3;
@@ -12,17 +16,38 @@ var vidaAtual = 3;
 function colisao() {
     const positionBody = body.getBoundingClientRect();
     const positionPlayer = player.getBoundingClientRect();
-    const positionEnemy = enemy.getBoundingClientRect();
+    const positionenemy01 = enemy01.getBoundingClientRect();
+    const positionenemy02 = enemy02.getBoundingClientRect();
+    const positionenemy03 = enemy03.getBoundingClientRect();
     const positionGameBoard = gameBoard.getBoundingClientRect();
 
     if (
         !(
             positionPlayer.right <
-                positionEnemy.left + positionGameBoard.left ||
+                positionenemy01.left + positionGameBoard.left ||
             positionPlayer.left >
-                positionEnemy.right + positionGameBoard.left ||
-            positionPlayer.bottom < positionEnemy.top + positionGameBoard.top ||
-            positionPlayer.top > positionEnemy.bottom + positionGameBoard.top
+                positionenemy01.right + positionGameBoard.left ||
+            positionPlayer.bottom <
+                positionenemy01.top + positionGameBoard.top ||
+            positionPlayer.top > positionenemy01.bottom + positionGameBoard.top
+        ) ||
+        !(
+            positionPlayer.right <
+                positionenemy02.left + positionGameBoard.left ||
+            positionPlayer.left >
+                positionenemy02.right + positionGameBoard.left ||
+            positionPlayer.bottom <
+                positionenemy02.top + positionGameBoard.top ||
+            positionPlayer.top > positionenemy02.bottom + positionGameBoard.top
+        ) ||
+        !(
+            positionPlayer.right <
+                positionenemy03.left + positionGameBoard.left ||
+            positionPlayer.left >
+                positionenemy03.right + positionGameBoard.left ||
+            positionPlayer.bottom <
+                positionenemy03.top + positionGameBoard.top ||
+            positionPlayer.top > positionenemy03.bottom + positionGameBoard.top
         )
     ) {
         vidaAtual--;
@@ -68,7 +93,7 @@ function moverBottom() {
     player.style.top = positionPlayer.top + 100 + "px";
 }
 function shoot() {
-    const enemy = document.querySelector(".enemy");
+    const enemy01 = document.querySelector(".enemy01");
     const bullet = document.querySelector(".bullet");
     bullet.style.cssText = " opacity:100%; top:-700px;";
     setTimeout(() => {
@@ -85,5 +110,23 @@ function score() {
         score.innerHTML = Number(num);
     }
 }
+function moveBackground() {
+    gameBoard.style.backgroundPosition = "bottom center";
+    gameBoard.style.backgroundPosition = "center center";
+    gameBoard.style.backgroundPosition = "top center";
+    gameBoard.style.backgroundPosition = "";
+}
+function enemyAnimations01() {
+    enemy01.style.animation = "enemy01 2s infinite ease-in-out";
+}
+function enemyAnimations02() {
+    enemy02.style.animation = "enemy02 2s infinite ease-in-out";
+}
+function enemyAnimations03() {
+    enemy03.style.animation = "enemy03 2s infinite ease-in-out";
+}
+setInterval(enemyAnimations01, 180);
+setInterval(enemyAnimations02, 900);
+setInterval(enemyAnimations03, 800);
 setInterval(score, 260);
 setInterval(colisao, 290);
