46dbf130dd0935e3d9edeee991a2e2ee520b554c
Matheus Iago <120859355+theus-iago@users.noreply.github.com>
2024-01-21 22:46:02 -0300

    update js/shop.js

--- js/shop.js
+++ js/shop.js
@@ -3,13 +3,13 @@ function buySkin02() {
   questShop.style.top = "8%";
   tituloShop.innerHTML = `Deseja mesmo comprar a skin Cyber?`;
   botaoYesShop.addEventListener("click", function() {
-    if(parseInt(localStorage.getItem('Coins')) < 100) {
-      alert("Valor insuficiente!");
-    }else {
+    if(parseInt(localStorage.getItem('Coins')) >= 100) {
       numCoin = parseInt(localStorage.getItem('Coins')) - 100;
       localStorage.setItem('Coins', numCoin);
       skin02State = "desbloqueada";
       localStorage.setItem('skin02', skin02State);
+    }else {
+      alert("Valor insuficiente!");
     }
   });
   botaoNoShop.addEventListener("click", function() {
@@ -19,15 +19,15 @@ function buySkin02() {
 function buySkin03() {
   questShop.style.display = "grid";
   questShop.style.top = "8%";
-  tituloShop.innerHTML = `Deseja mesmo comprar a skin Strong?`;
+  tituloShop.innerHTML = `Deseja mesmo comprar a skin Stronger?`;
   botaoYesShop.addEventListener("click", function() {
-    if(parseInt(localStorage.getItem('Coins')) < 180) {
-      alert("Valor insuficiente!");
-    }else {
+    if(parseInt(localStorage.getItem('Coins')) >= 180) {
       numCoin = parseInt(localStorage.getItem('Coins')) - 180;
       localStorage.setItem('Coins', numCoin);
       skin03State = "desbloqueada";
       localStorage.setItem('skin03', skin03State);
+    }else {
+      alert("Valor insuficiente!");
     }
   });
   botaoNoShop.addEventListener("click", function() {
@@ -39,13 +39,13 @@ function buySkin04() {
   questShop.style.top = "8%";
   tituloShop.innerHTML = `Deseja mesmo comprar a skin Slinder?`;
   botaoYesShop.addEventListener("click", function() {
-    if(parseInt(localStorage.getItem('Coins')) < 220) {
-      alert("Valor insuficiente!");
-    }else {
+    if(parseInt(localStorage.getItem('Coins')) >= 220) {
       numCoin = parseInt(localStorage.getItem('Coins')) - 220;
       localStorage.setItem('Coins', numCoin);
       skin04State = "desbloqueada";
       localStorage.setItem('skin04', skin04State);
+    }else {
+      alert("Valor insuficiente!");
     }
   });
   botaoNoShop.addEventListener("click", function() {
@@ -57,13 +57,13 @@ function buySkin05() {
   questShop.style.top = "8%";
   tituloShop.innerHTML = `Deseja mesmo comprar a skin Slinder?`;
   botaoYesShop.addEventListener("click", function() {
-    if(parseInt(localStorage.getItem('Coins')) < 250) {
-      alert("Valor insuficiente!");
-    }else {
+    if(parseInt(localStorage.getItem('Coins')) >= 250) {
       numCoin = parseInt(localStorage.getItem('Coins')) - 250;
       localStorage.setItem('Coins', numCoin);
       skin05State = "desbloqueada";
       localStorage.setItem('skin05', skin05State);
+    }else {
+      alert("Valor insuficiente!");
     }
   });
   botaoNoShop.addEventListener("click", function() {
@@ -75,13 +75,13 @@ function buySkin06() {
   questShop.style.top = "8%";
   tituloShop.innerHTML = `Deseja mesmo comprar a skin Treck?`;
   botaoYesShop.addEventListener("click", function() {
-    if(parseInt(localStorage.getItem('Coins')) < 300) {
-      alert("Valor insuficiente!");
-    }else {
+    if(parseInt(localStorage.getItem('Coins')) >= 300) {
       numCoin = parseInt(localStorage.getItem('Coins')) - 300;
       localStorage.setItem('Coins', numCoin);
       skin06State = "desbloqueada";
       localStorage.setItem('skin06', skin06State);
+    }else {
+      alert("Valor insuficiente!");
     }
   });
   botaoNoShop.addEventListener("click", function() {
@@ -93,16 +93,17 @@ function buySkin07() {
   questShop.style.top = "8%";
   tituloShop.innerHTML = `Deseja mesmo comprar a skin Flamp?`;
   botaoYesShop.addEventListener("click", function() {
-    if(parseInt(localStorage.getItem('Coins')) < 330) {
-      alert("Valor insuficiente!");
-    }else {
+    if(parseInt(localStorage.getItem('Coins')) >= 330) {
       numCoin = parseInt(localStorage.getItem('Coins')) - 330;
       localStorage.setItem('Coins', numCoin);
       skin07State = "desbloqueada";
       localStorage.setItem('skin07', skin07State);
+    }else {
+      alert("Valor insuficiente!");
     }
   });
   botaoNoShop.addEventListener("click", function() {
     questShop.style.display = "none";
   });
 }
+console.log(parseInt(localStorage.getItem('Coins')))
