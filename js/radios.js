84948236d4833735eaec7e72d67b2c1153f592a0
Matheus Iago <120859355+theus-iago@users.noreply.github.com>
2024-01-22 16:57:55 -0300

    update js/radios.js

--- js/radios.js
+++ js/radios.js
@@ -15,6 +15,7 @@ const radio07 = document.querySelector("#sk07");
 radioPadrao.checked = true
 
 function checkRadioPadrao() {
+const positionPlayer = player.getBoundingClientRect();
     if (radioPadrao.checked) {
     document.getElementsByClassName("preco")[0].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
       skinPadrao.style.cssText = "border: 5px solid #1e1e1e;";
@@ -34,50 +35,18 @@ function checkRadioPadrao() {
     }
 }
 function checkRadioPadrao02() {
+  const positionPlayer = player.getBoundingClientRect();
     if(localStorage.getItem('skin02') == "desbloqueada" && radio02.checked == false){
         document.getElementsByClassName("preco")[1].style.cssText = "background-color: blue; border-top: 5px solid blue;";
       skin02.style.cssText = "border: 5px solid blue;";
-        player.style.cssText = `background-image: url('./images/${radio02.value}.png');`;
-        document.getElementsByClassName("lifes")[0].src =
-            `./images/${radio02.value}.png`;
-        document.getElementsByClassName("lifes")[1].src =
-            `./images/${radio02.value}.png`;
-        document.getElementsByClassName("lifes")[2].src =
-            `./images/${radio02.value}.png`;
         img02.style.display = "none";
         preco02.innerHTML = "Cyber";
       }
     if(localStorage.getItem('skin02') == "desbloqueada" && radio02.checked == true){
         document.getElementsByClassName("preco")[1].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
       skin02.style.cssText = "border: 5px solid #1e1e1e;";
-        player.style.cssText = `background-image: url('./images/${radio02.value}.png');`;
-        document.getElementsByClassName("lifes")[0].src =
-            `./images/${radio02.value}.png`;
-        document.getElementsByClassName("lifes")[1].src =
-            `./images/${radio02.value}.png`;
-        document.getElementsByClassName("lifes")[2].src =
-            `./images/${radio02.value}.png`;
-        img02.style.display = "none";
-        preco02.innerHTML = "Cyber";
-      }
-}
-function checkRadioPadrao02() {
-    if(localStorage.getItem('skin02') == "desbloqueada" && radio02.checked == false){
-        document.getElementsByClassName("preco")[1].style.cssText = "background-color: blue; border-top: 5px solid blue;";
-      skin02.style.cssText = "border: 5px solid blue;";
-        player.style.cssText = `background-image: url('./images/${radio02.value}.png');`;
-        document.getElementsByClassName("lifes")[0].src =
-            `./images/${radio02.value}.png`;
-        document.getElementsByClassName("lifes")[1].src =
-            `./images/${radio02.value}.png`;
-        document.getElementsByClassName("lifes")[2].src =
-            `./images/${radio02.value}.png`;
-        img02.style.display = "none";
-        preco02.innerHTML = "Cyber";
-      }
-    if(localStorage.getItem('skin02') == "desbloqueada" && radio02.checked == true){
-        document.getElementsByClassName("preco")[1].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
-      skin02.style.cssText = "border: 5px solid #1e1e1e;";
+       player.style.top = positionPlayer.top;
+        player.style.left = positionPlayer.left;
         player.style.cssText = `background-image: url('./images/${radio02.value}.png');`;
         document.getElementsByClassName("lifes")[0].src =
             `./images/${radio02.value}.png`;
@@ -90,22 +59,18 @@ function checkRadioPadrao02() {
       }
 }
 function checkRadioPadrao03() {
+  const positionPlayer = player.getBoundingClientRect();
     if(localStorage.getItem('skin03') == "desbloqueada" && radio03.checked == false){
         document.getElementsByClassName("preco")[2].style.cssText = "background-color: blue; border-top: 5px solid blue;";
       skin03.style.cssText = "border: 5px solid blue;";
-        player.style.cssText = `background-image: url('./images/${radio03.value}.png');`;
-        document.getElementsByClassName("lifes")[0].src =
-            `./images/${radio03.value}.png`;
-        document.getElementsByClassName("lifes")[1].src =
-            `./images/${radio03.value}.png`;
-        document.getElementsByClassName("lifes")[2].src =
-            `./images/${radio03.value}.png`;
         img03.style.display = "none";
         preco03.innerHTML = "Slicky";
       }
     if(localStorage.getItem('skin03') == "desbloqueada" && radio03.checked == true){
         document.getElementsByClassName("preco")[2].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
       skin03.style.cssText = "border: 5px solid #1e1e1e;";
+        player.style.top = positionPlayer.top;
+        player.style.left = positionPlayer.left;
         player.style.cssText = `background-image: url('./images/${radio03.value}.png');`;
         document.getElementsByClassName("lifes")[0].src =
             `./images/${radio03.value}.png`;
@@ -118,22 +83,18 @@ function checkRadioPadrao03() {
       }
 }
 function checkRadioPadrao04() {
+  const positionPlayer = player.getBoundingClientRect();
     if(localStorage.getItem('skin04') == "desbloqueada" && radio04.checked == false){
         document.getElementsByClassName("preco")[3].style.cssText = "background-color: blue; border-top: 5px solid blue;";
       skin04.style.cssText = "border: 5px solid blue;";
-        player.style.cssText = `background-image: url('./images/${radio04.value}.png');`;
-        document.getElementsByClassName("lifes")[0].src =
-            `./images/${radio04.value}.png`;
-        document.getElementsByClassName("lifes")[1].src =
-            `./images/${radio04.value}.png`;
-        document.getElementsByClassName("lifes")[2].src =
-            `./images/${radio04.value}.png`;
         img04.style.display = "none";
         preco04.innerHTML = "Treck";
       }
     if(localStorage.getItem('skin04') == "desbloqueada" && radio04.checked == true){
         document.getElementsByClassName("preco")[3].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
       skin04.style.cssText = "border: 5px solid #1e1e1e;";
+       player.style.top = positionPlayer.top;
+        player.style.left = positionPlayer.left;
         player.style.cssText = `background-image: url('./images/${radio04.value}.png');`;
         document.getElementsByClassName("lifes")[0].src =
             `./images/${radio04.value}.png`;
@@ -146,22 +107,18 @@ function checkRadioPadrao04() {
       }
 }
 function checkRadioPadrao05() {
+  const positionPlayer = player.getBoundingClientRect();
     if(localStorage.getItem('skin05') == "desbloqueada" && radio05.checked == false){
         document.getElementsByClassName("preco")[4].style.cssText = "background-color: blue; border-top: 5px solid blue;";
       skin05.style.cssText = "border: 5px solid blue;";
-        player.style.cssText = `background-image: url('./images/${radio05.value}.png');`;
-        document.getElementsByClassName("lifes")[0].src =
-            `./images/${radio05.value}.png`;
-        document.getElementsByClassName("lifes")[1].src =
-            `./images/${radio05.value}.png`;
-        document.getElementsByClassName("lifes")[2].src =
-            `./images/${radio05.value}.png`;
         img05.style.display = "none";
         preco05.innerHTML = "Dig";
       }
     if(localStorage.getItem('skin05') == "desbloqueada" && radio05.checked == true){
         document.getElementsByClassName("preco")[4].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
       skin05.style.cssText = "border: 5px solid #1e1e1e;";
+        player.style.top = positionPlayer.top;
+        player.style.left = positionPlayer.left;
         player.style.cssText = `background-image: url('./images/${radio05.value}.png');`;
         document.getElementsByClassName("lifes")[0].src =
             `./images/${radio05.value}.png`;
@@ -174,22 +131,18 @@ function checkRadioPadrao05() {
       }
 }
 function checkRadioPadrao06() {
+  const positionPlayer = player.getBoundingClientRect();
     if(localStorage.getItem('skin06') == "desbloqueada" && radio06.checked == false){
         document.getElementsByClassName("preco")[5].style.cssText = "background-color: blue; border-top: 5px solid blue;";
       skin06.style.cssText = "border: 5px solid blue;";
-        player.style.cssText = `background-image: url('./images/${radio06.value}.png');`;
-        document.getElementsByClassName("lifes")[0].src =
-            `./images/${radio06.value}.png`;
-        document.getElementsByClassName("lifes")[1].src =
-            `./images/${radio06.value}.png`;
-        document.getElementsByClassName("lifes")[2].src =
-            `./images/${radio06.value}.png`;
         img06.style.display = "none";
         preco06.innerHTML = "Phitom";
       }
     if(localStorage.getItem('skin06') == "desbloqueada" && radio06.checked == true){
         document.getElementsByClassName("preco")[5].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
       skin06.style.cssText = "border: 5px solid #1e1e1e;";
+        player.style.top = positionPlayer.top;
+        player.style.left = positionPlayer.left;
         player.style.cssText = `background-image: url('./images/${radio06.value}.png');`;
         document.getElementsByClassName("lifes")[0].src =
             `./images/${radio06.value}.png`;
@@ -205,19 +158,16 @@ function checkRadioPadrao07() {
     if(localStorage.getItem('skin07') == "desbloqueada" && radio07.checked == false){
         document.getElementsByClassName("preco")[6].style.cssText = "background-color: blue; border-top: 5px solid blue;";
       skin07.style.cssText = "border: 5px solid blue;";
-        player.style.cssText = `background-image: url('./images/${radio07.value}.png');`;
-        document.getElementsByClassName("lifes")[0].src =
-            `./images/${radio07.value}.png`;
-        document.getElementsByClassName("lifes")[1].src =
-            `./images/${radio07.value}.png`;
-        document.getElementsByClassName("lifes")[2].src =
-            `./images/${radio07.value}.png`;
         img07.style.display = "none";
         preco07.innerHTML = "Volks";
       }
     if(localStorage.getItem('skin07') == "desbloqueada" && radio07.checked == true){
+      const player = document.querySelector("#player");
+  let positionPlayer = player.getBoundingClientRect();
         document.getElementsByClassName("preco")[6].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
       skin07.style.cssText = "border: 5px solid #1e1e1e;";
+       player.style.top = positionPlayer.top;
+        player.style.left = positionPlayer.left;
         player.style.cssText = `background-image: url('./images/${radio07.value}.png');`;
         document.getElementsByClassName("lifes")[0].src =
             `./images/${radio07.value}.png`;
@@ -229,13 +179,110 @@ function checkRadioPadrao07() {
         preco07.innerHTML = "Volks";
       }
 }
-setInterval(checkRadioPadrao,30);
-setInterval(checkRadioPadrao02,30);
-setInterval(checkRadioPadrao03,30);
-setInterval(checkRadioPadrao04,30);
-setInterval(checkRadioPadrao05,30);
-setInterval(checkRadioPadrao06,30);
-setInterval(checkRadioPadrao07,30);
+function unCheckPadrao() {
+  if(localStorage.getItem('skinPadrao') == "desbloqueada" && radioPadrao.checked == true) {
+    document.getElementsByClassName("preco")[0].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
+      skinPadrao.style.cssText = "border: 5px solid #1e1e1e;";
+    imgPadrao.style.display = "none";
+        precoPadrao.innerHTML = "Nebula";
+  }else if(localStorage.getItem('skinPadrao') == "desbloqueada" && radioPadrao.checked == false) {
+    document.getElementsByClassName("preco")[0].style.cssText = "background-color: blue; border-top: 5px solid blue;";
+      skinPadrao.style.cssText = "border: 5px solid blue;";
+    imgPadrao.style.display = "none";
+        precoPadrao.innerHTML = "Nebula";
+    
+  }
+}
+function unCheck02() {
+  if(localStorage.getItem('skin02') == "desbloqueada" && radio02.checked == true) {
+    document.getElementsByClassName("preco")[1].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
+      skin07.style.cssText = "border: 5px solid #1e1e1e;";
+    img02.style.display = "none";
+        preco02.innerHTML = "Cyber";
+  }else if(localStorage.getItem('skin02') == "desbloqueada" && radio02.checked == false) {
+    document.getElementsByClassName("preco")[1].style.cssText = "background-color: blue; border-top: 5px solid blue;";
+      skin02.style.cssText = "border: 5px solid blue;";
+    img02.style.display = "none";
+        preco02.innerHTML = "Cyber";
+    
+  }
+}
+function unCheck03() {
+  if(localStorage.getItem('skin03') == "desbloqueada" && radio03.checked == true) {
+    document.getElementsByClassName("preco")[2].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
+      skin03.style.cssText = "border: 5px solid #1e1e1e;";
+    img03.style.display = "none";
+        preco03.innerHTML = "Slicky";
+  }else if(localStorage.getItem('skin03') == "desbloqueada" && radio03.checked == false) {
+    document.getElementsByClassName("preco")[2].style.cssText = "background-color: blue; border-top: 5px solid blue;";
+      skin03.style.cssText = "border: 5px solid blue;";
+    img03.style.display = "none";
+        preco03.innerHTML = "Slicky";
+    
+  }
+}
+function unCheck04() {
+  if(localStorage.getItem('skin04') == "desbloqueada" && radio04.checked == true) {
+    document.getElementsByClassName("preco")[3].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
+      skin04.style.cssText = "border: 5px solid #1e1e1e;";
+    img04.style.display = "none";
+        preco04.innerHTML = "Treck";
+  }else if(localStorage.getItem('skin04') == "desbloqueada" && radio04.checked == false) {
+    document.getElementsByClassName("preco")[3].style.cssText = "background-color: blue; border-top: 5px solid blue;";
+      skin04.style.cssText = "border: 5px solid blue;";
+    img04.style.display = "none";
+        preco04.innerHTML = "Treck";
+    
+  }
+}
+function unCheck05() {
+  if(localStorage.getItem('skin05') == "desbloqueada" && radio05.checked == true) {
+    document.getElementsByClassName("preco")[4].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
+      skin05.style.cssText = "border: 5px solid #1e1e1e;";
+    img05.style.display = "none";
+        preco05.innerHTML = "Dig";
+  }else if(localStorage.getItem('skin05') == "desbloqueada" && radio05.checked == false) {
+    document.getElementsByClassName("preco")[4].style.cssText = "background-color: blue; border-top: 5px solid blue;";
+      skin05.style.cssText = "border: 5px solid blue;";
+    img05.style.display = "none";
+        preco05.innerHTML = "Dig";
+    
+  }
+}
+function unCheck06() {
+  if(localStorage.getItem('skin06') == "desbloqueada" && radio06.checked == true) {
+    document.getElementsByClassName("preco")[5].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
+      skin06.style.cssText = "border: 5px solid #1e1e1e;";
+    img06.style.display = "none";
+        preco06.innerHTML = "Phitom";
+  }else if(localStorage.getItem('skin06') == "desbloqueada" && radio06.checked == false) {
+    document.getElementsByClassName("preco")[5].style.cssText = "background-color: blue; border-top: 5px solid blue;";
+      skin06.style.cssText = "border: 5px solid blue;";
+    img06.style.display = "none";
+        preco06.innerHTML = "Phitom";
+    
+  }
+}
+function unCheck07() {
+  if(localStorage.getItem('skin07') == "desbloqueada" && radio07.checked == true) {
+    document.getElementsByClassName("preco")[6].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
+      skin07.style.cssText = "border: 5px solid #1e1e1e;";
+    img07.style.display = "none";
+        preco07.innerHTML = "Volks";
+  }else if(localStorage.getItem('skin07') == "desbloqueada" && radio07.checked == false) {
+    document.getElementsByClassName("preco")[6].style.cssText = "background-color: blue; border-top: 5px solid blue;";
+      skin07.style.cssText = "border: 5px solid blue;";
+    img07.style.display = "none";
+        preco07.innerHTML = "Volks";
+    
+  }
+}
+setInterval(unCheck02, 30);
+setInterval(unCheck03, 30);
+setInterval(unCheck04, 30);
+setInterval(unCheck05, 30);
+setInterval(unCheck06, 30);
+setInterval(unCheck07, 30);
 function checkRadio02() {
   if(radio02.checked) {
     console.log("selecionado")
@@ -412,19 +459,25 @@ function checkRadio07() {
   }
 radio02.addEventListener('click', function() {
   checkRadio02();
+  checkRadioPadrao02();
 });
 radio03.addEventListener('click', function() {
   checkRadio03();
+  checkRadioPadrao03();
 });
 radio04.addEventListener('click', function() {
   checkRadio04();
+  checkRadioPadrao04();
 });
 radio05.addEventListener('click', function() {
   checkRadio05();
+  checkRadioPadrao05();
 });
 radio06.addEventListener('click', function() {
   checkRadio06();
+  checkRadioPadrao06();
 });
 radio07.addEventListener('click', function() {
   checkRadio07();
+  checkRadioPadrao07();
 });
