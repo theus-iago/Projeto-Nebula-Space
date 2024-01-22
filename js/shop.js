7d871c34dd388c9ab045b24bb4fabeca919c45e0
Matheus Iago <120859355+theus-iago@users.noreply.github.com>
2024-01-21 21:54:13 -0300

    update js/shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,17 @@
 function buySkin02() {
   questShop.style.display = "grid";
   questShop.style.top = "8%";
-  tituloShop.innerHTML = `Deseja mesmo comprar a ${radio02.value}?`;
+  tituloShop.innerHTML = `Deseja mesmo comprar a skin Cyber?`;
+  botaoYesShop.addEventListener("click", function() {
+    if(parseInt(localStorage.getItem('Coins')) < 100) {
+      alert("Valor insuficiente!");
+    }else {
+      numCoin = parseInt(localStorage.getItem('Coins')) - 100;
+      localStorage.setItem('Coins', numCoin);
+      localStorage.setItem('skin02', unlock);
+    }
+  });
+  botaoNoShop.addEventListener("click", function() {
+    questShop.style.display = "none";
+  });
 }
