398e37b391e75a295afcb0b3d60c10cd50871270
Matheus Iago <120859355+theus-iago@users.noreply.github.com>
2024-01-24 12:22:37 -0300

    update js/radios.js

--- js/radios.js
+++ js/radios.js
@@ -1,40 +1,6 @@
-const skinPadrao = document.querySelector("#skinPadrao");
-const radioPadrao = document.querySelector("#skPadrao");
-const skin02 = document.querySelector("#skin02");
-const radio02 = document.querySelector("#sk02");
-const skin03 = document.querySelector("#skin03");
-const radio03 = document.querySelector("#sk03");
-const skin04 = document.querySelector("#skin04");
-const radio04 = document.querySelector("#sk04");
-const skin05 = document.querySelector("#skin05");
-const radio05 = document.querySelector("#sk05");
-const skin06 = document.querySelector("#skin06");
-const radio06 = document.querySelector("#sk06");
-const skin07 = document.querySelector("#skin07");
-const radio07 = document.querySelector("#sk07");
-let buy02 = false;
-let noBuy02 = true;
-let buy03 = false;
-let noBuy03 = true;
-let buy04 = false;
-let noBuy04 = true;
-let buy05 = false;
-let noBuy05 = true;
-let buy06 = false;
-let noBuy06 = true;
-let buy07 = false;
-let noBuy07 = true;
 function checkRadioPadrao() {
   const positionPlayer = player.getBoundingClientRect();
-    if(localStorage.getItem('skinPadrao') == "desbloqueada" && radioPadrao.checked == false){
-        document.getElementsByClassName("preco")[0].style.cssText = "background-color: blue; border-top: 5px solid blue;";
-      skinPadrao.style.cssText = "border: 5px solid blue;";
-        imgPadrao.style.display = "none";
-        precoPadrao.innerHTML = "Nebula";
-      }
     if(localStorage.getItem('skinPadrao') == "desbloqueada" && radioPadrao.checked == true){
-        document.getElementsByClassName("preco")[0].style.cssText = "background-color: #1e1e1e; border-top: 5px solid #1e1e1e;";
-      skinPadrao.style.cssText = "border: 5px solid #1e1e1e;";
        player.style.top = positionPlayer.top;
         player.style.left = positionPlayer.left;
         player.style.cssText = `background-image: url('./images/${radioPadrao.value}.png');`;
@@ -44,8 +10,6 @@ function checkRadioPadrao() {
             `./images/${radioPadrao.value}.png`;
         document.getElementsByClassName("lifes")[2].src =
             `./images/${radioPadrao.value}.png`;
-        imgPadrao.style.display = "none";
-        precoPadrao.innerHTML = "Nebula";
       }
 }
 function checkRadioPadrao02() {
