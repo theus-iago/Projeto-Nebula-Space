61dfb97f2015a06515daf7e5e773d941118b797c
Matheus Iago <120859355+theus-iago@users.noreply.github.com>
2024-01-30 10:34:12 -0300

    update js/movimentacao.js

--- js/movimentacao.js
+++ js/movimentacao.js
@@ -37,38 +37,41 @@ function move(direct) {
                 break;
         }
     }
-    /**
-     * Teste de correção de movimentação
-     * let positionGameBoard = window.getComputedStyle(gameBoard);
-    let positionPlayer = window.getComputedStyle(player);
-    switch (direct) {
-        case "left":
-            const numLeft = parseInt(positionPlayer.left);
-            player.style.left = numLeft + -100 + "px";
-            if (numLeft < parseInt(positionGameBoard.width)) {
+}
+document.addEventListener("keydown", function (event) {
+    let positionPlayer = player.getBoundingClientRect();
+    switch (event.key) {
+        case "ArrowLeft":
+        case "a":
+            const numLeft = Math.floor(positionPlayer.left);
+            player.style.left = positionPlayer.left + -100 + "px";
+            if (numLeft < 100) {
                 player.style.left = 0;
             }
             break;
-        case "right":
-            const numRight = parseInt(positionPlayer.right);
-            player.style.left = numRight + 100 + "px";
-            if (numRight > parseInt(positionGameBoard.width)) {
-                player.style.left = 100;
+        case "ArrowRight":
+        case "d":
+            const numRight = Math.floor(positionPlayer.right);
+            player.style.left = positionPlayer.left + 100 + "px";
+            if (numRight > 300) {
+                player.style.left = 78 + "%";
             }
             break;
-        case "top":
-            const numTop = parseInt(positionPlayer.top);
-            player.style.top = numTop + -100 + "px";
-            if (numTop < parseInt(positionGameBoard.height)) {
+        case "ArrowUp":
+        case "w":
+            const numTop = Math.floor(positionPlayer.top);
+            player.style.top = positionPlayer.top + -100 + "px";
+            if (numTop < 100) {
                 player.style.top = 0;
             }
             break;
-        case "bottom":
-            const numBottom = parseInt(positionPlayer.bottom);
-            player.style.top = numBottom + 100 + "px";
-            if (numBottom > parseInt(positionGameBoard.height)) {
-                player.style.top = 100;
+        case "ArrowDown":
+        case "s":
+            const numBottom = Math.floor(positionPlayer.bottom);
+            player.style.top = positionPlayer.top + 100 + "px";
+            if (numBottom > 300) {
+                player.style.top = 78 + "%";
             }
             break;
-    }*/
-}
+    }
+});
